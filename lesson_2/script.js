1bb5f63aee60ff4933a0d6473160b025861c23c0
GuVictory <gubanova.vi10@gmail.com>
2023-06-08 13:53:44 +0300

    lesson 3 added

---
+++ lesson_2/script.js
@@ -0,0 +1,28 @@
+// Сцена
+const scene = new THREE.Scene();
+
+// Объект
+const geometry = new THREE.BoxGeometry(1, 1, 1);
+const material = new THREE.MeshBasicMaterial({ color: 'purple' });
+const mesh = new THREE.Mesh(geometry, material);
+
+scene.add(mesh);
+
+// Камера
+const sizes = {
+    width: 600,
+    height: 600,
+};
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
+camera.position.z = 3;
+camera.position.y = 1;
+
+scene.add(camera);
+
+const canvas = document.querySelector('.canvas');
+
+const renderer = new THREE.WebGLRenderer({ canvas });
+
+renderer.setSize(sizes.width, sizes.height);
+
+renderer.render(scene, camera);
