4b16120ca5aa3b7497694eb0bb2501c47794b453
GuVictory <gubanova.vi10@gmail.com>
2023-06-14 20:37:59 +0300

    Lesson 4 added

--- threejs_main/src/script.js
+++ threejs_main/src/script.js
@@ -5,10 +5,24 @@ import './style.css';
 // Сцена
 const scene = new THREE.Scene();
 
+const axesHelper = new THREE.AxesHelper(3);
+
+scene.add(axesHelper);
+
 // Объект
 const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 'purple' });
+const material = new THREE.MeshBasicMaterial({
+    color: 'purple',
+    wireframe: true,
+});
 const mesh = new THREE.Mesh(geometry, material);
+// mesh.position.x = -1;
+// mesh.position.y = -0.8;
+// mesh.position.z = 0.5;
+
+mesh.scale.x = 0.5;
+mesh.scale.y = 2;
+mesh.scale.z = 0.7;
 
 scene.add(mesh);
 
@@ -17,9 +31,9 @@ const sizes = {
     width: 600,
     height: 600,
 };
+
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 camera.position.z = 3;
-camera.position.y = 1;
 
 scene.add(camera);
 
