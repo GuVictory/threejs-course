50210ecf2dcdadadca0b24844fe2128d5260e158
GuVictory <gubanova.vi10@gmail.com>
2023-07-09 10:43:22 +0300

    start for lesson 6 added

--- threejs_main/src/script.js
+++ threejs_main/src/script.js
@@ -16,38 +16,9 @@ const material = new THREE.MeshBasicMaterial({
     wireframe: true,
 });
 
-const group = new THREE.Group();
-group.scale.y = 1.4;
-group.rotation.x = Math.PI * 0.25;
+const mesh = new THREE.Mesh(geometry, material);
 
-const cube1 = new THREE.Mesh(geometry, material);
-cube1.position.x = -1.2;
-
-const cube2 = new THREE.Mesh(geometry, material);
-cube2.position.x = 0;
-
-const cube3 = new THREE.Mesh(geometry, material);
-cube3.position.x = 1.2;
-
-group.add(cube1);
-group.add(cube2);
-group.add(cube3);
-
-scene.add(group);
-
-// const mesh = new THREE.Mesh(geometry, material);
-// mesh.position.x = -1;
-// mesh.position.y = -0.8;
-// mesh.position.z = 0.5;
-
-// mesh.scale.x = 0.5;
-// mesh.scale.y = 2;
-// mesh.scale.z = 0.7;
-
-// mesh.rotation.x = Math.PI * 0.25;
-// mesh.rotation.y = Math.PI * 0.25;
-
-// scene.add(mesh);
+scene.add(mesh);
 
 // Камера
 const sizes = {
@@ -56,12 +27,9 @@ const sizes = {
 };
 
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.z = 5;
-camera.position.y = 1;
+camera.position.z = 3;
 
 scene.add(camera);
-
-// camera.lookAt(mesh.position);
 
 const canvas = document.querySelector('.canvas');
 
