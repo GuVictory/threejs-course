30024b9d16c0b3897aca2ba1c9298d7935e67016
GuVictory <gubanova.vi10@gmail.com>
2023-11-28 12:18:54 +0300

    lesson 13 start added

--- threejs_main/src/script.js
+++ threejs_main/src/script.js
@@ -15,59 +15,57 @@ const sizes = {
     width: window.innerWidth,
     height: window.innerHeight,
 };
-
-const parameters = {
-    color: 0xff0000,
-};
-
 const cursor = {
     x: 0,
     y: 0,
 };
 
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
-camera.position.z = 3;
+camera.position.z = 5;
+camera.position.y = 0.8;
 
 const controls = new OrbitControls(camera, canvas);
 controls.enableDamping = true;
 
 scene.add(camera);
 
-// Отладка
-const gui = new dat.GUI({ closeFolders: true, width: 400 });
+const gui = new dat.GUI({ closeFolders: true, width: 200 });
 
-const geometry = new THREE.BoxGeometry(1, 1, 1);
+const textureLoader = new THREE.TextureLoader();
+const color = textureLoader.load('/textures/lava/basecolor.jpg');
+const roughness = textureLoader.load('/textures/lava/roughness.jpg');
+const norm = textureLoader.load('/textures/lava/normal.jpg');
+const ao = textureLoader.load('/textures/lava/ambientOcclusion.jpg');
+const emissive = textureLoader.load('/textures/lava/emissive.jpg');
+const height = textureLoader.load('/textures/lava/height.png');
 
-const material = new THREE.MeshBasicMaterial({
-    color: parameters.color,
-    wireframe: true,
-});
-
+const geometry = new THREE.SphereGeometry(2, 100, 100);
+const material = new THREE.MeshStandardMaterial({});
 const mesh = new THREE.Mesh(geometry, material);
-
-const scaleFolder = gui.addFolder('Scale');
-scaleFolder.add(mesh.scale, 'x').min(0).max(5).step(0.1).name('Box scale x');
-scaleFolder.add(mesh.scale, 'y').min(0).max(5).step(0.1).name('Box scale y');
-scaleFolder.add(mesh.scale, 'z').min(0).max(5).step(0.1).name('Box scale z');
-
-gui.add(mesh, 'visible');
-gui.add(material, 'wireframe');
-gui.addColor(parameters, 'color').onChange(() => {
-    material.color.set(parameters.color);
-});
-
 scene.add(mesh);
+
+
+
+
+const light = new THREE.AmbientLight(0xefefef, 1.5);
+const pointLight = new THREE.PointLight(0xff9000, 3);
+pointLight.position.set(3, 3, 3);
+const pointLight2 = new THREE.PointLight(0x000000, 6);
+pointLight2.position.set(-3, -3, 3);
+
+scene.add(light);
+scene.add(pointLight);
+scene.add(pointLight2);
 
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(sizes.width, sizes.height);
 renderer.render(scene, camera);
-
 const clock = new THREE.Clock();
 
 const tick = () => {
     stats.begin();
     const delta = clock.getDelta();
-    // mesh.rotation.y += delta * 0.4;
+    mesh.rotation.y += delta * 0.2;
 
     controls.update();
     renderer.render(scene, camera);
@@ -93,10 +91,3 @@ window.addEventListener('resize', () => {
     renderer.render(scene, camera);
 });
 
-window.addEventListener('dblclick', () => {
-    if (!document.fullscreenElement) {
-        canvas.requestFullscreen();
-    } else {
-        document.exitFullscreen();
-    }
-});
